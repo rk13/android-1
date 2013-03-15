cdad6b56d1ee4fbfbe3ddd96c63d01734e3e57e7
Aleksejs Truhans <svires@gmail.com>
2013-03-15 11:21:36 +0100

    Documented

--- AsyncHttp/src/com/example/asynchttp/MainActivity.java
+++ AsyncHttp/src/com/example/asynchttp/MainActivity.java
@@ -4,17 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
+/**
+ * Demo getting JSON over HTTP in a background task of an Android Activity. 
+ * Be warned that it uses HttpURLConnection, which was somewhat buggy prior to 2.3. 
+ * It also uses getLastNonConfigurationInstance() instead of the recommended 
+ * Fragment.setRetainInstance() for simplicity sake.
+ * 
+ * Put your service URL in HTTP_URL constant. It shall be something producing a line 
+ * like this: 
+ *     { "patient": { "name":"Samuel", "surname":"Vimes" } } 
+ * 
+ * @author AleksejsTruhans
+ * 
+ */
 public class MainActivity extends Activity {
 
+	private static final String HTTP_URL = "http://172.18.1.94:8080/AndroidBackend/DataService";
 	private TextView responseView;
 	private HttpTask httpTask;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		responseView = (TextView) findViewById(R.id.responseView);
+		// Google recommends using Fragment.setRetainInstance(),
+		// yet it seems too complex for this demonstration purposes
 		httpTask = (HttpTask) getLastNonConfigurationInstance();  
 	}
 	
@@ -26,7 +42,7 @@ public class MainActivity extends Activity {
 	private void launchHttpRequest() {
 		httpTask = new HttpTask(); 
 		httpTask.setResponseView(responseView);	
-		httpTask.execute("http://172.18.1.94:8080/AndroidBackend/DataService");
+		httpTask.execute(HTTP_URL);
 	}
 
 	@Override
@@ -48,11 +64,6 @@ public class MainActivity extends Activity {
 		if (httpTask != null) { 
 			httpTask.setResponseView(responseView);
 		}
-	}
-
-	
-	
-	
-	
+	}	
 }
 
